8e6b5f8c3a613174a8ef99fa507c41a8f5ed9373
Gogs <gogs@fake.local>
2023-10-05 16:27:14 +0200

    Дфи23

---
+++ Lab23/Program.cs
@@ -0,0 +1,68 @@
+﻿//Уровень базовый
+//Console.Write("Введите n:");
+//int n=int.Parse(Console.ReadLine());
+//switch(n)
+//{
+//    case 1:
+//        {
+//            int a = 46, b = 74;
+//            Console.WriteLine($"Произведение цифр:{a*b}");
+//        }
+//        break;
+//    case 2:
+//        {
+//            int a = 41, b = 81;
+//            Console.WriteLine($"Произведение цифр:{a * b}");
+//        }
+//        break;
+//    case 3:
+//        {
+//            int a = 39, b = 76;
+//            Console.WriteLine($"Произведение цифр:{a * b}");
+//        }
+//        break;
+//    case 4:
+//        {
+//            int a = 29, b = 53;
+//            Console.WriteLine($"Произведение цифр:{a * b}");
+//        }
+//        break;
+//}
+//Средний уровень 26
+Console.Write("Введите n:");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите n:");
+double x = double.Parse(Console.ReadLine());
+switch (n)
+{
+    case 1:
+        {
+            double z = Math.Asin(x * x * x);
+            double y=0;
+            if (z > -0.5) y = ((2 * z + 1) * (2 * z + 1)) / (3.71 - x * x);
+            else if (z > -0.5 && z <= 1E-3) y = Math.Pow(Math.Sin(z), 3) - Math.Sin(z / 3 * Math.PI);
+            else y = (Math.Tan(z + x) - Math.Exp(x)) / (3.5*x);
+            Console.WriteLine($"y={y:F2}");
+        }
+        break;
+    case 2:
+        {
+            double z = Math.Acos(x * x * x);
+            double y = 0;
+            if (z > -0.5) y = ((2 * z + 1) * (2 * z + 1)) / (3.71 - x * x);
+            else if (z > -0.5 && z <= 1E-3) y = Math.Pow(Math.Sin(z), 3) - Math.Sin(z / 3 * Math.PI);
+            else y = (Math.Tan(z + x) - Math.Exp(x)) / (3.5 * x);
+            Console.WriteLine($"y={y:F2}");
+        }
+        break;
+    case 3:
+        {
+            double z = Math.Tan(x * x * x);
+            double y = 0;
+            if (z > -0.5) y = ((2 * z + 1) * (2 * z + 1)) / (3.71 - x * x);
+            else if (z > -0.5 && z <= 1E-3) y = Math.Pow(Math.Sin(z), 3) - Math.Sin(z / 3 * Math.PI);
+            else y = (Math.Tan(z + x) - Math.Exp(x)) / (3.5 * x);
+            Console.WriteLine($"y={y:F2}");
+        }
+        break;
+}
---
+++ Lesson16/Program.cs
@@ -0,0 +1,38 @@
+﻿//Console.Write("Введите номер дня недели:");
+//int n=int.Parse(Console.ReadLine());
+//switch(n)
+//{
+//    case 1: Console.WriteLine("Понедельник");break;
+//    case 2: Console.WriteLine("Вторник"); break;
+//    case 3: Console.WriteLine("Среда"); break;
+//    case 4: Console.WriteLine("Четверг"); break;
+//    case 5: Console.WriteLine("Пятница"); break;
+//    case 6: Console.WriteLine("Суббота"); break;
+//    case 7: Console.WriteLine("Воскресенье"); break;
+//    default: Console.WriteLine("Такого дня недели нет");break;
+//}
+//Console.Write("Введите номер месяца:");
+//int n = int.Parse(Console.ReadLine());
+//switch (n)
+//{
+//    case 12: case 1: case 2: Console.WriteLine("Зима"); break;
+//    case 3: case 4: case 5: Console.WriteLine("Весна"); break;
+//    case 6: case 7: case 8: Console.WriteLine("Лето");break;
+//    case 9: case 10: case 11: Console.WriteLine("Осень"); break;
+//    default: Console.WriteLine("Такого месяца нет"); break;
+//}
+
+//if(n>=1&&n<=31) Console.WriteLine("Январь");
+//else if(n>=32&&n<=58) Console.WriteLine("Февраль");
+//else if (n >= 59 && n <= 90) Console.WriteLine("Март");
+
+Console.Write("Введите сумму:");
+int n = int.Parse(Console.ReadLine());
+if(n%100>10&&n%100<15) Console.WriteLine(n + " рублей");
+else
+switch (n%10)
+{
+    case 1: Console.WriteLine(n+" рубль");break;
+    case 2: case 3: case 4: Console.WriteLine(n + " рубля"); break;
+    default: Console.WriteLine(n + " рублей"); break;
+}
