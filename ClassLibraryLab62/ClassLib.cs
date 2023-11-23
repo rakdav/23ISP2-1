b2bd9791e667b7fb55621cefed27cc0e727c57c0
Gogs <gogs@fake.local>
2023-11-23 11:12:15 +0200

    Lab62Lib

---
+++ ClassLibraryLab62/ClassLib.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLibraryLab62
+{
+    public class ClassLib
+    {
+        public static int[] Negative(int[,] mas)
+        {
+            int[] result=new int[mas.GetLength(0)];
+            for(int i=0;i<mas.GetLength(0);i++)
+            {
+                int count = 0;
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    if (mas[i, j] < 0) count++;
+                }
+                result[i] = count;
+            }
+            return result;
+        }
+    }
+}
---
+++ Lab62/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibraryLab62;
+try
+{
+    Console.Write("Введите n:");
+    int n=int.Parse(Console.ReadLine());
+    Console.Write("Введите m:");
+    int m = int.Parse(Console.ReadLine());
+    int[,] mas=new int[n,m];
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i, j] = new Random().Next(-100, 100);
+            Console.Write(mas[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+    int[] result = ClassLibraryLab62.ClassLib.Negative(mas);
+    for (int i = 0;i < result.Length; i++) 
+    {
+        Console.Write(result[i]+" ");
+    }
+    Console.WriteLine();
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
