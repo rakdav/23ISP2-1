8344631b8e40a15ac4ac712dbae93fc30632304e
Gogs <gogs@fake.local>
2023-12-14 16:37:24 +0200

    Lab10

---
+++ Lab10/Program.cs
@@ -0,0 +1,46 @@
+﻿Cylinders cs=new Cylinders(4,10,3);
+cs.Print();
+class Cylinder
+{
+    private double radius;
+    private double height;
+    public double Radius
+    {
+        get { return radius; }
+        set { radius = value; }
+    }
+    public double Height
+    {
+        get { return height; }
+        set { height = value; }
+    }
+    public Cylinder(double radius, double height)
+    {
+        this.radius = radius;
+        this.height = height;
+    }
+    public double getArea()
+    {
+        return 2 * Math.PI * radius * height + 2 * Math.PI * radius * radius;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Радиус:{radius} Высота:{height} Площадь:{getArea():F2}");
+    }
+}
+class Cylinders : Cylinder
+{
+    public int Count { get; set; }
+    public Cylinders(double radius, double height,int count) : base(radius, height)
+    {
+        Count = count;
+    }
+    public double AllArea()
+    {
+        return Count*getArea();
+    }
+    public new void Print()
+    {
+        Console.WriteLine($"Радиус:{Radius} Высота:{Height} Количество:{Count} Площадь:{AllArea():F2}");
+    }
+}
---
+++ Lesson41/Program.cs
@@ -0,0 +1,31 @@
+﻿Person person = new Person("Вася", 23);
+Console.WriteLine(person);
+Employee employee=new Employee("Боря",25,"Samsung");
+Console.WriteLine(employee);
+class Person
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public Person(string? name,int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public override string? ToString()
+    {
+        return $"Имя:{Name} Возраст:{Age}";
+    }
+}
+class Employee : Person
+{
+    public string? Company { get; set; }
+    public Employee(string? name, int age, string? company) : base(name, age)
+    {
+        Company = company;
+    }
+
+    public override string? ToString()
+    {
+        return base.ToString()+$" Компания:{Company}";
+    }
+}
